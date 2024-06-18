e44eecd5089430ede0bf06549d6ac2547704ddd2
Henry Nyobange <henrynyobange@gmail.com>
2024-06-18 22:04:12 +0300

    Update index.js

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,12 +1,13 @@
-import AppController from '../controllers/AppController';
-import StudentsController from '../controllers/StudentsController';
+import { AppController } from '../controllers/AppController';
+import { StudentsController } from '../controllers/StudentsController';
 
+const express = require('express');
+const router = express.Router();
 
-const mapRoutes = (app) => {
-  app.get('/', AppController.getHomepage);
-  app.get('/students', StudentsController.getAllStudents);
-  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
-};
+// Route for the homepage
+router.get('/', AppController.getHomepage);
 
-export default mapRoutes;
-module.exports = mapRoutes;
+// Route for getting all students or students by major
+router.get('/students/:major?', StudentsController.getAllStudents);
+
+export default router;
